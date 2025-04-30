66e075de5b562d9ca89e1c0508aca2e42e2faec7
Andrii Filipenko <andreyfil1337@gmail.com>
2025-04-30 11:23:08 +0300

    solution

--- src/figuresClasses.test.ts
+++ src/figuresClasses.test.ts
@@ -1,95 +1,94 @@
-// import { Triangle, Rectangle, Circle, getInfo } from './figuresClasses';
-import { Circle } from './figuresClasses';
+import { Triangle, Rectangle, Circle, getInfo } from './figuresClasses';
 
 describe('Class "Triangle"', () => {
-//   test('should be declared', () => {
-//     expect(Triangle)
-//       .toBeInstanceOf(Function);
-//   });
+  test('should be declared', () => {
+    expect(Triangle)
+      .toBeInstanceOf(Function);
+  });
 
-  //   test('should create an instance of class Triangle', () => {
-  //     const triangle = new Triangle('green', 3, 4, 5);
+  test('should create an instance of class Triangle', () => {
+    const triangle = new Triangle('green', 3, 4, 5);
 
-  //     expect(triangle)
-  //       .toBeInstanceOf(Triangle);
-  //   });
+    expect(triangle)
+      .toBeInstanceOf(Triangle);
+  });
 
-  //   test('should have a public property "shape"', () => {
-  //     const triangle = new Triangle('green', 3, 4, 5);
+  test('should have a public property "shape"', () => {
+    const triangle = new Triangle('green', 3, 4, 5);
 
-  //     expect(triangle)
-  //       .toHaveProperty('shape');
-  //   });
+    expect(triangle)
+      .toHaveProperty('shape');
+  });
 
-  //   test('should have a public property "color"', () => {
-  //     const triangle = new Triangle('green', 3, 4, 5);
+  test('should have a public property "color"', () => {
+    const triangle = new Triangle('green', 3, 4, 5);
 
-  //     expect(triangle)
-  //       .toHaveProperty('color');
-  //   });
+    expect(triangle)
+      .toHaveProperty('color');
+  });
 
-  //   test('should check whether all sides are positive numbers', () => {
-  //     expect(() => {
-  //       new Triangle('green', 0, 2, 2);
-  //     }).toThrow(Error);
-  //   });
+  test('should check whether all sides are positive numbers', () => {
+    expect(() => {
+      new Triangle('green', 0, 2, 2);
+    }).toThrow(Error);
+  });
 
-  //   test('should check whether it is a triangle', () => {
-  //     expect(() => {
-  //       new Triangle('green', 5, 10, 15);
-  //     }).toThrow(Error);
-  //   });
+  test('should check whether it is a triangle', () => {
+    expect(() => {
+      new Triangle('green', 5, 10, 15);
+    }).toThrow(Error);
+  });
 
-  //   test('should have a method "getArea"', () => {
-  //     const triangle = new Triangle('green', 3, 4, 5);
+  test('should have a method "getArea"', () => {
+    const triangle = new Triangle('green', 3, 4, 5);
 
-  //     expect(triangle.getArea)
-  //       .toBeInstanceOf(Function);
-  //   });
-  // });
+    expect(triangle.getArea)
+      .toBeInstanceOf(Function);
+  });
+});
 
-  // describe('Method "getArea" of class "Triangle"', () => {
-  //   test('should return a number', () => {
-  //     const triangle = new Triangle('green', 3, 4, 5);
+describe('Method "getArea" of class "Triangle"', () => {
+  test('should return a number', () => {
+    const triangle = new Triangle('green', 3, 4, 5);
 
-  //     expect(typeof triangle.getArea())
-  //       .toBe('number');
-  //   });
+    expect(typeof triangle.getArea())
+      .toBe('number');
+  });
 
-  //   test('should return a correct square of a triangle', () => {
-  //     const triangle = new Triangle('green', 3, 4, 5);
+  test('should return a correct square of a triangle', () => {
+    const triangle = new Triangle('green', 3, 4, 5);
 
-  //     expect(triangle.getArea())
-  //       .toBe(6);
-  //   });
-  // });
+    expect(triangle.getArea())
+      .toBe(6);
+  });
+});
 
-  // describe('Class "Circle"', () => {
-  //   test('should be declared', () => {
-  //     expect(Circle)
-  //       .toBeInstanceOf(Function);
-  //   });
+describe('Class "Circle"', () => {
+  test('should be declared', () => {
+    expect(Circle)
+      .toBeInstanceOf(Function);
+  });
 
-  //   test('should create an instance of class Circle', () => {
-  //     const circle = new Circle('red', 6);
+  test('should create an instance of class Circle', () => {
+    const circle = new Circle('red', 6);
 
-  //     expect(circle)
-  //       .toBeInstanceOf(Circle);
-  //   });
+    expect(circle)
+      .toBeInstanceOf(Circle);
+  });
 
-  //   test('should have a public property "shape"', () => {
-  //     const circle = new Circle('red', 6);
+  test('should have a public property "shape"', () => {
+    const circle = new Circle('red', 6);
 
-  //     expect(circle)
-  //       .toHaveProperty('shape');
-  //   });
+    expect(circle)
+      .toHaveProperty('shape');
+  });
 
-  //   test('should have a public property "color"', () => {
-  //     const circle = new Circle('red', 6);
+  test('should have a public property "color"', () => {
+    const circle = new Circle('red', 6);
 
-  //     expect(circle)
-  //       .toHaveProperty('color');
-  //   });
+    expect(circle)
+      .toHaveProperty('color');
+  });
 
   test('should check whether radius is a positive number', () => {
     expect(() => {
@@ -97,118 +96,118 @@ describe('Class "Triangle"', () => {
     }).toThrow(Error);
   });
 
-  //   test('should have a method "getArea"', () => {
-  //     const circle = new Circle('red', 6);
+  test('should have a method "getArea"', () => {
+    const circle = new Circle('red', 6);
 
-  //     expect(circle.getArea)
-  //       .toBeInstanceOf(Function);
-  //   });
-  // });
+    expect(circle.getArea)
+      .toBeInstanceOf(Function);
+  });
+});
 
-  // describe('Method "getArea" of class "Circle"', () => {
-  //   test('should return a number', () => {
-  //     const circle = new Circle('red', 6);
+describe('Method "getArea" of class "Circle"', () => {
+  test('should return a number', () => {
+    const circle = new Circle('red', 6);
 
-  //     expect(typeof circle.getArea())
-  //       .toBe('number');
-  //   });
+    expect(typeof circle.getArea())
+      .toBe('number');
+  });
 
-  //   test('should return a correct square of a circle', () => {
-  //     const circle = new Circle('red', 6);
+  test('should return a correct square of a circle', () => {
+    const circle = new Circle('red', 6);
 
-  //     expect(circle.getArea())
-  //       .toBe(113.09);
-  //   });
-  // });
+    expect(circle.getArea())
+      .toBe(113.09);
+  });
+});
 
-  // describe('Class "Rectangle"', () => {
-  //   test('should be declared', () => {
-  //     expect(Rectangle)
-  //       .toBeInstanceOf(Function);
-  //   });
+describe('Class "Rectangle"', () => {
+  test('should be declared', () => {
+    expect(Rectangle)
+      .toBeInstanceOf(Function);
+  });
 
-  //   test('should create an instance of class Rectangle', () => {
-  //     const rectangle = new Rectangle('blue', 8, 10);
+  test('should create an instance of class Rectangle', () => {
+    const rectangle = new Rectangle('blue', 8, 10);
 
-  //     expect(rectangle)
-  //       .toBeInstanceOf(Rectangle);
-  //   });
+    expect(rectangle)
+      .toBeInstanceOf(Rectangle);
+  });
 
-  //   test('should have a public property "shape"', () => {
-  //     const rectangle = new Rectangle('blue', 8, 10);
+  test('should have a public property "shape"', () => {
+    const rectangle = new Rectangle('blue', 8, 10);
 
-  //     expect(rectangle)
-  //       .toHaveProperty('shape');
-  //   });
+    expect(rectangle)
+      .toHaveProperty('shape');
+  });
 
-  //   test('should have a public property "color"', () => {
-  //     const rectangle = new Rectangle('blue', 8, 10);
+  test('should have a public property "color"', () => {
+    const rectangle = new Rectangle('blue', 8, 10);
 
-  //     expect(rectangle)
-  //       .toHaveProperty('color');
-  //   });
+    expect(rectangle)
+      .toHaveProperty('color');
+  });
 
-  //   test('should check whether all sides are positive numbers', () => {
-  //     expect(() => {
-  //       new Rectangle('blue', 5, -1);
-  //     }).toThrow(Error);
-  //   });
+  test('should check whether all sides are positive numbers', () => {
+    expect(() => {
+      new Rectangle('blue', 5, -1);
+    }).toThrow(Error);
+  });
 
-  //   test('should have a method "getArea"', () => {
-  //     const rectangle = new Rectangle('blue', 8, 10);
+  test('should have a method "getArea"', () => {
+    const rectangle = new Rectangle('blue', 8, 10);
 
-  //     expect(rectangle.getArea)
-  //       .toBeInstanceOf(Function);
-  //   });
-  // });
+    expect(rectangle.getArea)
+      .toBeInstanceOf(Function);
+  });
+});
 
-  // describe('Method "getArea" of class "Rectangle"', () => {
-  //   test('should return a number', () => {
-  //     const rectangle = new Rectangle('blue', 8, 10);
+describe('Method "getArea" of class "Rectangle"', () => {
+  test('should return a number', () => {
+    const rectangle = new Rectangle('blue', 8, 10);
 
-  //     expect(typeof rectangle.getArea())
-  //       .toBe('number');
-  //   });
+    expect(typeof rectangle.getArea())
+      .toBe('number');
+  });
 
-  //   test('should return a correct square of a rectangle', () => {
-  //     const rectangle = new Rectangle('blue', 8, 10);
+  test('should return a correct square of a rectangle', () => {
+    const rectangle = new Rectangle('blue', 8, 10);
 
-  //     expect(rectangle.getArea())
-  //       .toBe(80);
-  //   });
-  // });
+    expect(rectangle.getArea())
+      .toBe(80);
+  });
+});
 
-  // describe('Function "getInfo"', () => {
-  //   test('should be declared', () => {
-  //     expect(getInfo)
-  //       .toBeInstanceOf(Function);
-  //   });
+describe('Function "getInfo"', () => {
+  test('should be declared', () => {
+    expect(getInfo)
+      .toBeInstanceOf(Function);
+  });
 
-  //   test('should return a string', () => {
-  //     const blueTriangle = new Triangle('blue', 6, 7, 8);
+  test('should return a string', () => {
+    const blueTriangle = new Triangle('blue', 6, 7, 8);
 
-  //     expect(typeof getInfo(blueTriangle))
-  //       .toBe('string');
-  //   });
+    expect(typeof getInfo(blueTriangle))
+      .toBe('string');
+  });
 
-  //   test('should return correct information about a triangle', () => {
-  //     const redTriangle = new Triangle('red', 10, 12, 15);
+  test('should return correct information about a triangle', () => {
+    const redTriangle = new Triangle('red', 10, 12, 15);
 
-  //     expect(getInfo(redTriangle))
-  //       .toBe('A red triangle - 59.81');
-  //   });
+    expect(getInfo(redTriangle))
+      .toBe('A red triangle - 59.81');
+  });
 
-  //   test('should return correct information about a circle', () => {
-  //     const greenCircle = new Circle('green', 13.31);
+  test('should return correct information about a circle', () => {
+    const greenCircle = new Circle('green', 13.31);
 
-  //     expect(getInfo(greenCircle))
-  //       .toBe('A green circle - 556.55');
-  //   });
+    expect(getInfo(greenCircle))
+      .toBe('A green circle - 556.55');
+  });
 
-  //   test('should return correct information about a rectangle', () => {
-  //     const blueRectangle = new Rectangle('blue', 9, 17);
+  test('should return correct information about a rectangle', () => {
+    const blueRectangle = new Rectangle('blue', 9, 17);
 
-//     expect(getInfo(blueRectangle))
-//       .toBe('A blue rectangle - 153');
-//   });
+    expect(getInfo(blueRectangle))
+      .toBe('A blue rectangle - 153');
+  });
 });
--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,12 +1,11 @@
 /* eslint-disable no-useless-constructor */
-/* eslint-disable @typescript-eslint/lines-between-class-members */
 type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 
 export interface Figure {
   shape: Shape;
   color: Color;
-  getArea(): number;
+  getArea(): number | Error;
 }
 
 export class Triangle implements Figure {
@@ -16,9 +15,7 @@ export class Triangle implements Figure {
     public b: number,
     public c: number,
     public shape: Shape = 'triangle',
-  ) {}
-
-  getArea(): number {
+  ) {
     const sides = [this.a, this.b, this.c].sort(
       (side1, side2) => side2 - side1,
     );
@@ -28,13 +25,15 @@ export class Triangle implements Figure {
       throw new Error('Wrong sides value.');
     } else if (longestSide >= sides[0] + sides[1]) {
       throw new Error('Wrong sides value.');
-    } else {
-      const p = (this.a + this.b + this.c) * 0.5;
-
-      return Number(
-        Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(2),
-      );
     }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) * 0.5;
+
+    return Number(
+      Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(2),
+    );
   }
 }
 
@@ -43,14 +42,14 @@ export class Circle implements Figure {
     public color: Color,
     public radius: number,
     public shape: Shape = 'circle',
-  ) {}
-
-  getArea(): number {
+  ) {
     if (this.radius <= 0) {
       throw new Error('Wrong radius value');
-    } else {
-      return Number((Math.PI * Math.pow(this.radius, 2)).toFixed(2));
     }
+  }
+
+  getArea(): number {
+    return Number((Math.PI * Math.pow(this.radius, 2)).toFixed(2));
   }
 }
 
@@ -60,14 +59,14 @@ export class Rectangle implements Figure {
     public width: number,
     public height: number,
     public shape: Shape = 'rectangle',
-  ) {}
-
-  getArea(): number {
+  ) {
     if (this.width <= 0 || this.height <= 0) {
       throw new Error('Wrong sides value.');
-    } else {
-      return Number((this.width * this.height).toFixed(2));
     }
+  }
+
+  getArea(): number | Error {
+    return Number((this.width * this.height).toFixed(2));
   }
 }
 
